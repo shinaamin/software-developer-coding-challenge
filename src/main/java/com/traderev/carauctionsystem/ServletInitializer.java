718dabb1a627ee811037c8be1cd436fc07c9acc3
shina amin <shina.mahajan@interfacing.com>
2018-08-02 18:54:24 -0400

    Project Setup using spring boot

---
+++ src/main/java/com/traderev/carauctionsystem/ServletInitializer.java
@@ -0,0 +1,11 @@
+package com.traderev.carauctionsystem;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CarAuctionSystemApplication.class);
+	}
+}
