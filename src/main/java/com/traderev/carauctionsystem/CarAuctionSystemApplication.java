06b2f5eb227ec8e3dda3a42157d2c7458edbd68d
shina amin <shina.mahajan@interfacing.com>
2018-08-02 18:53:35 -0400

    Project Setup using spring boot

---
+++ src/main/java/com/traderev/carauctionsystem/CarAuctionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.traderev.carauctionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarAuctionSystemApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CarAuctionSystemApplication.class, args);
+	}
+}
