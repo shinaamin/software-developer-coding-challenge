85d30cb37c52f7263b17e783ac87a9434542da76
shina amin <shina.mahajan@interfacing.com>
2018-08-10 18:38:56 -0400

    Errors Fixed

--- src/main/java/com/traderev/carauctionsystem/controller/BidController.java
+++ src/main/java/com/traderev/carauctionsystem/controller/BidController.java
@@ -1,5 +1,9 @@
 package com.traderev.carauctionsystem.controller;
 
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Controller class to handle Bid related web requests.
  *
--- src/main/java/com/traderev/carauctionsystem/controller/CarController.java
+++ src/main/java/com/traderev/carauctionsystem/controller/CarController.java
@@ -1,6 +1,10 @@
 package com.traderev.carauctionsystem.controller;
 
 
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Controller class to handle car related web requests.
  *
